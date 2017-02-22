fba7c18986d2967b7cd7d5d715602b8128936f8a
sajit <sajit.kunnumkal@gmail.com>
2017-02-21 23:01:09 -0500

    qs intro

---
+++ src/main/resources/static/ng/src/app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by sajit on 2/21/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent { name = 'Angular'; }
---
+++ src/main/resources/static/ng/src/app/app.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by sajit on 2/21/17.
+ */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
